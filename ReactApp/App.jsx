fce2aaac21e252d1db6c74d1dc0459852fc39d4d
Jieyang Qian <jieyangqian@sllaptop-54.corp.syslogicinc.com>
2017-09-15 16:34:09 -0500

    Reusable Components

--- ReactApp/App.jsx
+++ ReactApp/App.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import Button from './Button.jsx'
+import Result from './Result.jsx'
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World changed again!!!
-         </div>
-      );
-   }
+    constructor(props) {
+        super(props);
+
+        this.state = { counter: 0 };
+        // This binding is necessary to make `this` work in the callback
+        this.IncrementCounter = this.IncrementCounter.bind(this);
+    }
+
+    IncrementCounter (value) {
+        this.setState((prevState) => ({
+             counter: prevState.counter + value
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                Hello World!
+                <div>
+                    <Button incrementValue={1} onClickFunction={this.IncrementCounter}/>
+                    <Button incrementValue={5} onClickFunction={this.IncrementCounter}/>
+                    <Button incrementValue={10} onClickFunction={this.IncrementCounter}/>
+                    <Button incrementValue={20} onClickFunction={this.IncrementCounter}/>
+                    <Result counter={this.state.counter}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ ReactApp/Result.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Result(props) {
+    return <div>{props.counter}</div>;
+}
+
+export default Result;
